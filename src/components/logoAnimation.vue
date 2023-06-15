493a5a6e7e2c17dacede290784f60dacedcef191
YF_x <2089046083@qq.com>
2023-06-16 02:50:42 +0800

    add FOZU

--- src/components/logoAnimation.vue
+++ src/components/logoAnimation.vue
@@ -3,21 +3,21 @@ import loadJson from '../assets/Json/data.json'
 </script>
 
 <template>
-  <div class="logoAnim" :class="{loadAnimation : Value}">
-    <Vue3Lottie :animationData="loadJson" loop="false"/>
+  <div class="logoAnim">
+    <Vue3Lottie :animationData="loadJson"/>
   </div>
 </template>
 
 <style>
-  .logoAnim{
-    width: 50%;
-    height: auto;
-    position: absolute;
-    top: 20%;
-    right: 0;
-        bottom: 0;
-        left: 0;
-        margin: auto;
-        overflow: auto;
-  }
+.logoAnim {
+  width: 50%;
+  height: auto;
+  position: absolute;
+  top: 20%;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  overflow: auto;
+}
 </style>
