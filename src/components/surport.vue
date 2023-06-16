a5cdb320203712f3ca43180be8c08b26cd7770ba
YF_x <2089046083@qq.com>
2023-06-17 01:21:38 +0800

    updata img url

--- src/components/surport.vue
+++ src/components/surport.vue
@@ -1,17 +1,17 @@
 <script setup>
 const surportLists = [
-    { name: "GPotta", url: "https://www.freefonts.io/graham-potta-font/", imgurl: "../src/assets/surpLogo/font.png", descript: "英文字体" },
-    { name: "阿里妈妈数黑体", url: "https://www.iconfont.cn/fonts/detail?spm=a313x.7781069.1998910419.d9df05512&cnid=a9fXc2HD9n7s", imgurl: "../src/assets/surpLogo/font.png", descript: "中文字体" },
-    { name: "Vite", url: "https://cn.vitejs.dev/", imgurl: "../src/assets/surpLogo/vite.png", descript: "下一代的前端工具链" },
-    { name: "Vue3", url: "https://cn.vuejs.org/", imgurl: "../src/assets/surpLogo/Vue.png", descript: "渐进式前端框架" },
-    { name: "VScode", url: "https://code.visualstudio.com/", imgurl: "../src/assets/surpLogo/vscode.png", descript: "前端开发者你必须要知道的工具" },
-    { name: "Vercel", url: "https://vercel.com/", imgurl: "../src/assets/surpLogo/vercel.png", descript: "一键部署你的项目" },
-    { name: "Git", url: "https://git-scm.com/", imgurl: "../src/assets/surpLogo/git.png", descript: "最好的代码管理工具" },
-    { name: "Github", url: "https://github.com/", imgurl: "../src/assets/surpLogo/github.png", descript: "全球最大交友平台" },
-    { name: "Nodejs", url: "https://nodejs.org/en", imgurl: "../src/assets/surpLogo/Nodejs.png", descript: "最好的版本控制工具" },
-    { name: "Lottie", url: "https://lottiefiles.com/", imgurl: "../src/assets/surpLogo/lottie.png", descript: "从JSON到动画,强大的动画库" },
-    { name: "Bodymo", url: "https://aescripts.com/bodymovin/", imgurl: "../src/assets/surpLogo/puli.png", descript: "把你充满创意的动画导出为JSON的AE插件" },
-    { name: "AE", url: "https://www.adobe.com/cn/products/aftereffects.html", imgurl: "../src/assets/surpLogo/Ae.png", descript: "制作气势恢宏的大场面" },
+    { name: "GPotta", url: "https://www.freefonts.io/graham-potta-font/", imgurl: "https://i.imgtg.com/2023/06/17/OTN79I.png", descript: "英文字体" },
+    { name: "阿里妈妈数黑体", url: "https://www.iconfont.cn/fonts/detail?spm=a313x.7781069.1998910419.d9df05512&cnid=a9fXc2HD9n7s", imgurl: "https://i.imgtg.com/2023/06/17/OTN79I.png", descript: "中文字体" },
+    { name: "Vite", url: "https://cn.vitejs.dev/", imgurl: "https://i.imgtg.com/2023/06/17/OTNgIg.png", descript: "下一代的前端工具链" },
+    { name: "Vue3", url: "https://cn.vuejs.org/", imgurl: "https://i.imgtg.com/2023/06/17/OTNphB.png", descript: "渐进式前端框架" },
+    { name: "VScode", url: "https://code.visualstudio.com/", imgurl: "https://i.imgtg.com/2023/06/17/OTNyEl.png", descript: "前端开发者你必须要知道的工具" },
+    { name: "Vercel", url: "https://vercel.com/", imgurl: "https://i.imgtg.com/2023/06/17/OTNveb.png", descript: "一键部署你的项目" },
+    { name: "Git", url: "https://git-scm.com/", imgurl: "https://i.imgtg.com/2023/06/17/OTN1R1.png", descript: "最好的代码管理工具" },
+    { name: "Github", url: "https://github.com/", imgurl: "https://i.imgtg.com/2023/06/17/OTNnFG.png", descript: "全球最大交友平台" },
+    { name: "Nodejs", url: "https://nodejs.org/en", imgurl: "https://i.imgtg.com/2023/06/17/OTNAd6.png", descript: "最好的版本控制工具" },
+    { name: "Lottie", url: "https://lottiefiles.com/", imgurl: "https://i.imgtg.com/2023/06/17/OTNeUD.png", descript: "从JSON到动画,强大的动画库" },
+    { name: "Bodymo", url: "https://aescripts.com/bodymovin/", imgurl: "https://i.imgtg.com/2023/06/17/OTNZaP.png", descript: "把你充满创意的动画导出为JSON的AE插件" },
+    { name: "AE", url: "https://www.adobe.com/cn/products/aftereffects.html", imgurl: "https://i.imgtg.com/2023/06/17/OTNryF.png", descript: "制作气势恢宏的大场面" },
 ]
 </script>
 
