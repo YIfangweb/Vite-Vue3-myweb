7401bf97aeb42e14bd0ae2e3b87eb66f83d0e366
YF_x <2089046083@qq.com>
2023-06-18 12:47:38 +0800

    add vercle

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Vue3Lottie from 'vue3-lottie'
 import router from './router'
+import { inject } from '@vercel/analytics';
+ 
+
+inject();
 
 const app = createApp(App);
 
 
 app.use(router);
 app.use(Vue3Lottie);
-
 
 app.mount('#app');
